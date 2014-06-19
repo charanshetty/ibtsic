a426a5a95f7caefe3fc97b0a40f2d436e6d8d1fb
Dnyanesh Joshi <dnyanesh.joshi@iiitb.org>
2014-06-20 01:17:17 +0530

    Added code to the Announcement model class.

--- IbtsicClientUser/src/org/iiitb/ibtsicclientuser/model/Announcement.java
+++ IbtsicClientUser/src/org/iiitb/ibtsicclientuser/model/Announcement.java
@@ -2,5 +2,21 @@ package org.iiitb.ibtsic.action.model;
 
 public class Announcement
 {
-
+	public int id;
+	public String name;
+	public String description;
+	public String startInstant;
+	public String validity;
+	public int nodeId;
+	
+	public Announcement(int id, String name, String description, String startInstant,
+			String validity, int nodeId)
+	{
+		this.id=id;
+		this.name=name;
+		this.description=description;
+		this.startInstant=startInstant;
+		this.validity=validity;
+		this.nodeId=nodeId;
+	}
 }
