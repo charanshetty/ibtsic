bab68d3fad6972ead359196c0b69126f265f3cc8
Dnyanesh Joshi <dnyanesh.joshi@iiitb.org>
2014-06-26 10:22:30 +0530

    schema updated

--- Documents/Deliverables/DB/ibtsic.sql
+++ Documents/Deliverables/DB/ibtsic.sql
@@ -58,10 +58,13 @@ CREATE TABLE `Bus` (
   `longitude` double DEFAULT '0',
   `onwardPathId` int(11) DEFAULT NULL,
   `returnPathId` int(11) DEFAULT NULL,
+  `currentPathId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `regNo_UNIQUE` (`regNo`),
   KEY `fk_Bus_1` (`onwardPathId`),
   KEY `fk_Bus_2` (`returnPathId`),
+  KEY `fk_Bus_3` (`currentPathId`),
+  CONSTRAINT `fk_Bus_3` FOREIGN KEY (`currentPathId`) REFERENCES `Path` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_Bus_1` FOREIGN KEY (`onwardPathId`) REFERENCES `Path` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_Bus_2` FOREIGN KEY (`returnPathId`) REFERENCES `Path` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
@@ -73,7 +76,6 @@ CREATE TABLE `Bus` (
 
 LOCK TABLES `Bus` WRITE;
 /*!40000 ALTER TABLE `Bus` DISABLE KEYS */;
-INSERT INTO `Bus` VALUES (1,'r1',1.11,2.22,3,5),(3,'f',0,0,1,2);
 /*!40000 ALTER TABLE `Bus` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +93,7 @@ CREATE TABLE `Node` (
   `longitude` double DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `Nodecol_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +102,7 @@ CREATE TABLE `Node` (
 
 LOCK TABLES `Node` WRITE;
 /*!40000 ALTER TABLE `Node` DISABLE KEYS */;
-INSERT INTO `Node` VALUES (1,'JP Nagar 6th Phase',1.11,2.22),(2,'Ganga Nagara',3.33,4.44),(7,'tyyrytr',2,4),(13,'jhjh',22,22),(14,'hgfhgfhfgfhgf',44,44),(15,'hgfhgf',77,77),(16,'nbmnb',777,78),(19,'rtyq',7.01,8.02),(21,'hghjg',8,8),(22,'ghfhgfhff',22,1),(23,'hgfghfhgfhgfghf',1,2);
+INSERT INTO `Node` VALUES (30,'n1',0,0),(31,'n2',0,0),(32,'n3',0,0),(33,'n4',0,0),(34,'n5',0,0),(35,'n6',0,0);
 /*!40000 ALTER TABLE `Node` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +118,7 @@ CREATE TABLE `Path` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +127,7 @@ CREATE TABLE `Path` (
 
 LOCK TABLES `Path` WRITE;
 /*!40000 ALTER TABLE `Path` DISABLE KEYS */;
-INSERT INTO `Path` VALUES (1,'MBS-1.onward'),(2,'MBS-1.return'),(3,'route1.onward'),(5,'route1.return'),(7,'route3');
+INSERT INTO `Path` VALUES (8,'p1'),(11,'p1r'),(9,'p2'),(10,'p3');
 /*!40000 ALTER TABLE `Path` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,7 +151,7 @@ CREATE TABLE `PathNode` (
   KEY `fk_PathNode_2` (`nodeId`),
   CONSTRAINT `fk_PathNode_1` FOREIGN KEY (`pathId`) REFERENCES `Path` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_PathNode_2` FOREIGN KEY (`nodeId`) REFERENCES `Node` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -158,7 +160,7 @@ CREATE TABLE `PathNode` (
 
 LOCK TABLES `PathNode` WRITE;
 /*!40000 ALTER TABLE `PathNode` DISABLE KEYS */;
-INSERT INTO `PathNode` VALUES (1,1,1,1,NULL,NULL,0),(2,1,2,2,NULL,NULL,0),(3,2,2,1,NULL,NULL,0),(4,2,1,2,NULL,NULL,0),(5,3,1,1,NULL,NULL,0),(8,5,1,1,NULL,NULL,0),(11,7,2,1,NULL,NULL,0),(12,7,7,2,NULL,NULL,0);
+INSERT INTO `PathNode` VALUES (13,8,30,1,NULL,'00:00:00',0),(14,8,31,2,'01:00:00','01:00:00',0),(15,8,32,3,NULL,NULL,0),(16,8,33,4,'04:00:00',NULL,0),(17,9,34,1,NULL,'00:00:00',0),(18,9,31,2,'01:10:00','01:10:00',0),(19,9,32,3,NULL,NULL,0),(20,9,33,4,'03:00:00',NULL,0),(21,9,35,5,'04:00:00',NULL,0),(22,10,30,1,NULL,NULL,0),(23,10,31,2,NULL,NULL,0),(24,10,35,3,NULL,NULL,0),(25,11,33,1,NULL,NULL,0),(26,11,32,2,NULL,NULL,0),(27,11,31,3,NULL,NULL,0),(28,11,30,4,NULL,NULL,0);
 /*!40000 ALTER TABLE `PathNode` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,7 +180,7 @@ CREATE TABLE `Run` (
   PRIMARY KEY (`id`),
   KEY `fk_Run_1` (`pathId`),
   CONSTRAINT `fk_Run_1` FOREIGN KEY (`pathId`) REFERENCES `Path` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,7 +189,7 @@ CREATE TABLE `Run` (
 
 LOCK TABLES `Run` WRITE;
 /*!40000 ALTER TABLE `Run` DISABLE KEYS */;
-INSERT INTO `Run` VALUES (1,1,'06:40:00','07:40:00',1),(2,2,'09:00:00','10:10:00',1),(3,3,'14:25:00','15:35:00',1),(4,4,'17:20:00','18:30:00',1),(5,5,'19:50:00','21:00:00',1),(6,1,'07:45:00','08:55:00',2),(7,2,'10:40:00','11:50:00',2),(8,3,'16:05:00','17:15:00',2),(9,4,'18:35:00','19:45:00',2),(10,5,'21:05:00','22:05:00',2),(11,1,'08:00:00','09:00:00',5),(12,2,'08:10:00','09:10:00',5),(13,1,'08:00:00','09:00:00',7);
+INSERT INTO `Run` VALUES (14,1,'19:00:00','21:00:00',8),(15,2,'20:00:00','22:00:00',8),(16,1,'20:00:00','22:00:00',9),(17,2,'20:00:00','22:00:00',9),(18,1,'20:00:00','22:00:00',10),(19,1,'20:00:00','22:00:00',11);
 /*!40000 ALTER TABLE `Run` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -200,4 +202,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-24 21:03:53
+-- Dump completed on 2014-06-26 10:20:39
