601275a1f4c8fea8300f06bf7494c64092ab838d
Dnyanesh Joshi <dnyanesh.joshi@iiitb.org>
2014-07-03 19:45:18 +0530

    added servlet to fetch active announcements

--- IbtsicServer/src/org/iiitb/ibtsic/action/model/Announcement.java
+++ IbtsicServer/src/org/iiitb/ibtsic/action/model/Announcement.java
@@ -10,16 +10,18 @@ public class Announcement
 	public String description;
 	public String startInstant;
 	public String validity;
-	public int nodeId;
+	public int node1Id;
+	public int node2Id;
 	
 	public Announcement(int id, String name, String description, String startInstant,
-			String validity, int nodeId)
+			String validity, int node1Id, int node2Id)
 	{
 		this.id=id;
 		this.name=name;
 		this.description=description;
 		this.startInstant=startInstant;
 		this.validity=validity;
-		this.nodeId=nodeId;
+		this.node1Id=node1Id;
+		this.node2Id=node2Id;
 	}
 }
